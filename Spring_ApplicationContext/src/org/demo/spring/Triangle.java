2e66dd94185412bcd4ee9e40384ad9cc8a59eaf5
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-22 08:08:58 +0530

    Add files via upload

---
+++ Spring_ApplicationContext/src/org/demo/spring/Triangle.java
@@ -0,0 +1,21 @@
+package org.demo.spring;
+
+public class Triangle {
+    private String type;
+    
+    
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public void draw(){
+		System.out.println(getType() +  "Triangle drawn");
+	}
+	
+}
---
+++ Spring_ConstructorInjection/src/org/demo/spring/Triangle.java
@@ -0,0 +1,45 @@
+package org.demo.spring;
+
+public class Triangle {
+    private String type;
+    private int height;
+    
+    public Triangle(String type) {
+		this.type = type;
+	}
+	
+	public Triangle(int height) {
+		this.height = height;
+	}
+
+	public Triangle(String type, int height) {
+		super();
+		this.type = type;
+		this.height = height;
+	}
+
+	 
+    public String getType() {
+		return type;
+	}
+	
+    public int getHeight() {
+		return height;
+	}
+
+	/*public void setHeight(int height) {
+		this.height = height;
+	}
+*/
+	
+ 
+	/*public void setType(String type) {
+		this.type = type;
+	}
+*/
+
+	public void draw(){
+		System.out.println(getType() +  " Triangle drawn at height " +getHeight());
+	}
+	
+}
