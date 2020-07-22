cd32aace0def4518f06281736e4c9afcd38770d6
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-22 08:05:32 +0530

    Add files via upload

---
+++ SpringDemo/src/org/demo/spring/Triangle.java
@@ -0,0 +1,9 @@
+package org.demo.spring;
+
+public class Triangle {
+
+	public void draw(){
+		System.out.println("Triangle drawn");
+	}
+	
+}
---
+++ Spring_InitializingCollections/src/org/demo/spring/Triangle.java
@@ -0,0 +1,28 @@
+package org.demo.spring;
+
+import java.util.List;
+
+public class Triangle {
+    
+	private List<Point> points;
+	
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+
+	public void draw() {
+   
+		for(Point point : points){
+			System.out.println("Point A ={" +point.getX() +", "+point.getY() + "}");
+		}
+		
+				
+
+	}
+	
+	
+}
